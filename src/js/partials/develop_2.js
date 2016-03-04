9d206088fa0b1bcdd3697d94294b5dd2b810e828
Art Garm <artem.garmatyk@gmail.com>
2016-03-04 12:51:16 +0200

    fdx

--- src/js/partials/develop_2.js
+++ src/js/partials/develop_2.js
@@ -1,7 +1,21 @@
 try{
 
     $(document).ready(function(){
+        $('.row-tool li').click(function() {
+            console.log($(this).index());
+            $('.cactuz').addClass('none-visible');
 
+            if ($(this).index() == 0) {
+                $('.family1').removeClass('none-visible');
+            };
+            if ($(this).index() == 1) {
+                $('.family2').removeClass('none-visible');
+            };
+            if ($(this).index() == 2) {
+                $('.family3').removeClass('none-visible');
+            };
+            $('#izdelie2').trigger('refresh');
+        });
     });
 
     $(window).load(function(){
