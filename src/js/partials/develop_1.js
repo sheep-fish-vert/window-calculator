13cb8c883b9d6d97638f90b6fb13b8659c33b6f5
Dmitry <dmitry1939@gmail.com>
2016-03-07 11:48:14 +0200

    window-calculator checkpoint

--- src/js/partials/develop_1.js
+++ src/js/partials/develop_1.js
@@ -16,20 +16,181 @@ try{
             $('.calc-window-img>ul>li').eq(parentIndex).addClass('active');
             $('.calc-window-img>ul>li.active li').eq(itemIndex).addClass('active');
 
+            if(parentIndex == 3){
+                $('.door-drag-inputs').addClass('show');
+                $('.slide-width-door, .slide-height-door').addClass('show');
+                $('#slide-width-door').val($('#slide-width-door').data('min'));
+                $('#slide-height-door').val($('#slide-height-door').data('min'));
+            }else{
+               $('.door-drag-inputs').removeClass('show');
+               $('.slide-width-door, .slide-height-door').removeClass('show');
+               $('#slide-width-door').val(0);
+               $('#slide-height-door').val(0);
+            }
+
         });
 
         $('.calculator-tabs-item').eq(0).find('label').eq(0).click();
 
     }
 
+    /* init slide */
+
+    function sliderInput(){
+
+        $('.slider-inp-item').each(function(index) {
+
+            var slider = $(this).attr('id');
+            var sliderMax = $(this).data('max');
+            var sliderMin = $(this).data('min');
+            var sliderStep = $(this).data('step');
+            var sliderVal = $(this).val();
+            var sliderOrientation = $(this).data('orientation');
+
+            $('.slider-item.'+slider).slider({
+                range:"max",
+                min:sliderMin,
+                max:sliderMax,
+                step:sliderStep,
+                value:sliderVal,
+                orientation:sliderOrientation,
+                slide:function(evente, ui){
+                    $('.slide-input-item #'+slider).val(ui.value);
+                    calcLogic();
+                }
+            });
+
+        });
+
+    }
+
+    function numberWithSpaces(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    }
+
+    /* calculator logic */
+
+    function calcLogic(){
+
+        var chossenProfile = atributesForProgrammer[$('.row-tool li.active').data('profile')][$('.cactuz:not(.none-visible) option:selected').index()];
+
+        var steklopaketCena = chossenProfile.steklopaket[$('#izdelie4 option:checked').index()];
+
+        var tihiySteklopaket = chossenProfile.steklopaketTishina[0];
+        if($('.silence-check input:checked').length){
+            tihiySteklopaket = chossenProfile.steklopaketTishina[1];
+        }
+
+        var furnituraOkna = chossenProfile.furnitura[$('.calculator-tabs-item.active input:checked').data('furnitura')];
+        var impostCena = chossenProfile.impostPrice[$('.calculator-tabs-item.active input:checked').data('impost')];
+        var ramaCena = chossenProfile.ramaPrice;
+        var stvorkaCena = chossenProfile.stvorkaPrice;
+
+        var windowWidth = $('#slide-width-window').val();
+        var windowHeight = $('#slide-height-window').val();
+        var doorWidth = $('#slide-width-door').val();
+        var doorHeight = $('#slide-height-door').val();
+
+
+
+        /* rama price by formuls */
+        var ramaAllPrice = (((windowWidth+windowHeight)*2)/1000)*ramaCena;
+
+        /* stvorki price by formuls */
+        var stvorkiAllPrice = ((((windowWidth/2)+windowHeight)*2)/1000)*stvorkaCena;
+
+        /* steklopaket price by formuls */
+        var steklopaketAllPrice = (windowWidth/1000)*(windowHeight/1000)*(steklopaketCena+tihiySteklopaket);
+
+        /* verticalImpost price by formuls */
+        var verticalImpost = (windowHeight/1000)*impostCena;
+
+        var horizontalImpost = 0;
+        var sendwichPrice = 0;
+        if($('.door-drag-inputs').is('.show')){
+
+            /* horizontalImpost price by formuls */
+            horizontalImpost = (doorWidth/1000)*impostCena;
+
+            /* sendwich price by formuls */
+            sendwichPrice = ((doorHeight - doorWidth)/1000)*horizontalImpost*chossenProfile.sendwichCena;
+        }
+
+        var allPrice = numberWithSpaces(parseInt(ramaAllPrice + stvorkiAllPrice + steklopaketAllPrice + verticalImpost + horizontalImpost+sendwichPrice));
+
+        $('.straday-res').text(allPrice);
+
+    }
+
+    /* /calculator logic */
+
+    /* call logic function when change, radio or checkbox */
+
+    function callCalculatorLogic(){
+
+        $('.contact-form-item select').change(function(){
+
+            calcLogic();
+
+        });
+
+        $('.contact-form-item input[type="checkbox"]').change(function(){
+
+            calcLogic();
+
+        });
+
+        $('.checkers input[type="checkbox"]').change(function(){
+
+            calcLogic();
+
+        });
+
+        $('.calculator-tabs input[type="radio"]').change(function(){
+
+            calcLogic();
+
+        });
+
+        $('.slide-input input').keypress(function(e){
+
+            if (e.which >= 47 && e.which <= 57 ){}
+            else return false;
+
+        });
+
+        $('.slide-input input').blur(function() {
+
+            var id = $(this).attr('id');
+            $inputNum = $(this);
+
+            if ($inputNum.val == '' || $inputNum.val() < $inputNum.data('min')) {
+                $inputNum.val($inputNum.data('min'));
+            }
+            else if($inputNum.val() > $inputNum.data('max')){
+                $inputNum.val($inputNum.data('max'));
+            }
+
+            $('.'+id).slider('value', $(this).val());
+            calcLogic();
+        });
+
+    }
+
+    /* /call logic function when change radio or checkbox */
+
 
     $(document).ready(function(){
 
+        sliderInput();
         tabsClick();
 
     });
 
     $(window).load(function(){
+
+        calcLogic();
+        callCalculatorLogic();
 
     });
 
--- src/js/partials/develop_4.js
+++ src/js/partials/develop_4.js
@@ -1,121 +1,12 @@
 try{
 
-    function sliderInput(){
 
-        var input = [];
-
-        $('.slider-inp-item').each(function(index) {
-                input[index] = {
-                    max:$(this).data('max'),
-                    min:$(this).data('min'),
-                    step:$(this).data('step'),
-                    val:$(this).val(),
-                    orientation:$(this).data('orientation')
-                };
-        });
-
-        $('.slider-item').each(function(index) {
-
-
-            $(this).slider({
-                range: "max",
-                min: input[index].min,
-                max: input[index].max,
-                step: input[index].step,
-                value: input[index].val,
-                orientation:input[index].orientation,
-                slide: function( event, ui ) {
-                    $('.slide-input-item[data-slider='+index+']').find('input[type=text]').val( ui.value);
-                    calcLogic();
-                }
-            });
-
-        });
-
-    }
-
-    function numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-    }
-
-    /* calculator logic */
-
-    function calcLogic(){
-
-        var chossenProfile = atributesForProgrammer[$('.row-tool li.active').data('profile')][$('.cactuz:not(.none-visible) option:selected').index()];
-
-        var steklopaketCena = chossenProfile.steklopaket[$('#izdelie4 option:checked').index()];
-
-        var tihiySteklopaket = chossenProfile.steklopaketTishina[0];
-        if($('.silence-check input:checked').length){
-            tihiySteklopaket = chossenProfile.steklopaketTishina[1];
-        }
-
-        var furnituraOkna = chossenProfile.furnitura[$('.calculator-tabs-item.active input:checked').data('furnitura')];
-        var impostCena = chossenProfile.impostPrice[$('.calculator-tabs-item.active input:checked').data('impost')];
-        var ramaCena = chossenProfile.ramaPrice;
-        var stvorkaCena = chossenProfile.stvorkaPrice;
-
-        var windowWidth = $('#slide-width').val();
-        var windowHeight = $('#slide-height').val();
-
-        var ramaAllPrice = (((windowWidth+windowHeight)*2)/1000)*ramaCena; /* rama price by formuls */
-        var stvorkiAllPrice = ((((windowWidth/2)+windowHeight)*2)/1000)*stvorkaCena; /* stvorki price by formuls */
-        var steklopaketAllPrice = (windowWidth/1000)*(windowHeight/1000)*(steklopaketCena+tihiySteklopaket); /* steklopaket price by formuls */
-        var verticalImpost = (windowHeight/1000)*impostCena;
-        var horizontalImpost = (windowWidth/1000)*impostCena;
-        /* sendwichCena */
-
-        var allPrice = numberWithCommas(parseInt(ramaAllPrice + stvorkiAllPrice + steklopaketAllPrice + verticalImpost + horizontalImpost));
-
-        $('.straday-res').text(allPrice);
-
-    }
-
-    /* /calculator logic */
-
-    /* call logic function when change, radio or checkbox */
-
-    function callCalculatorLogic(){
-
-        $('.contact-form-item select').change(function(){
-
-            calcLogic();
-
-        });
-
-        $('.contact-form-item input[type="checkbox"]').change(function(){
-
-            calcLogic();
-
-        });
-
-        $('.checkers input[type="checkbox"]').change(function(){
-
-            calcLogic();
-
-        });
-
-        $('.calculator-tabs input[type="radio"]').change(function(){
-
-            calcLogic();
-
-        });
-
-    }
-
-    /* /call logic function when change radio or checkbox */
 
     $(document).ready(function(){
-
-        sliderInput();
 
     });
 
     $(window).load(function(){
-
-        calcLogic();
-        callCalculatorLogic();
 
     });
 
